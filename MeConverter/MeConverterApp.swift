ebab3fe9e96ffaf3ba2faac1aff8201ed2c74fb9
Niezwan <nzwnabdulwahid@gmail.com>
2022-07-21 23:00:35 +0800

    Initial Commit

---
+++ MeConverter/MeConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeConverterApp.swift
+//  MeConverter
+//
+//  Created by Niezwan Abdul Wahid on 21/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct MeConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
