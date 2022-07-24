7d581466693615732207410852f1332c36d470de
Niezwan <nzwnabdulwahid@gmail.com>
2022-07-24 18:16:06 +0800

    First Push

--- MeConverter/ContentView.swift
+++ MeConverter/ContentView.swift
@@ -8,9 +8,75 @@
 import SwiftUI
 
 struct ContentView: View {
+
+    @State private var inputUnit:Dimension = UnitTemperature.fahrenheit
+    @State private var outputUnit:Dimension = UnitTemperature.kelvin
+    @State private var inputValue = 0.0
+    @State private var activeUnits = 0;
+   
+//    Dynamic Converter
+    let listOfUnits = ["Temperature", "Length", "Time", "Volume"];
+    let listOfMeasurements = [
+        [UnitTemperature.fahrenheit, UnitTemperature.kelvin, UnitTemperature.celsius],
+        [UnitLength.meters,UnitLength.kilometers, UnitLength.yards, UnitLength.feet,UnitLength.miles],
+        [UnitDuration.seconds, UnitDuration.minutes, UnitDuration.hours],
+        [UnitVolume.liters, UnitVolume.cups, UnitVolume.pints, UnitVolume.gallons]
+    ]
+//    Formatter
+    let formatter: MeasurementFormatter
+    var result: String {
+        let inputMes = Measurement(value: inputValue, unit: inputUnit);
+        let outputMes = inputMes.converted(to: outputUnit);
+        
+        return formatter.string(from: outputMes)
+    }
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView{
+            Form {
+                Section{
+                    Picker("Units", selection: $activeUnits){
+                        ForEach(0..<listOfUnits.count, id: \.self){
+                            Text(listOfUnits[$0])
+                        }
+                    }
+                }
+                
+                Section {
+                    TextField("Value", value: $inputValue, format: .number).keyboardType(.decimalPad)
+                }
+                Section{
+                    
+                    Picker("Convert From", selection: $inputUnit){
+                        ForEach(listOfMeasurements[activeUnits], id: \.self){
+                            Text(formatter.string(from: $0).capitalized)
+                        }
+                    }
+                    
+                    Picker("Convert To", selection: $outputUnit){
+                        ForEach(listOfMeasurements[activeUnits], id: \.self){
+                            Text(formatter.string(from: $0).capitalized)
+                        }
+                    }
+                }
+                
+                Section {
+                    
+                    Text("\(result)")
+                }
+            }.navigationTitle("MyConverter")
+                .onChange(of: activeUnits){ newActiveUnits in
+                    let units = listOfMeasurements[newActiveUnits]
+                    inputUnit = units[0];
+                    outputUnit = units[1];
+                }
+        }
+    
+    }
+    
+    init(){
+        formatter = MeasurementFormatter()
+        formatter.unitStyle = .long
+        formatter.unitOptions = .providedUnit
     }
 }
 
